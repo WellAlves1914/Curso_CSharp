4bade53e3723df154359857c7d68b49f296bae86
wellington.aalves4 <wellington.aalves4@L130576511W10-1>
2025-01-13 22:06:42 -0300

    Adicionar arquivos de projeto.

---
+++ Ex004/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Ex004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            float num4, resposta;
+
+            Console.WriteLine("Digite um número;");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            num3 = num1 + num2;
+
+            Console.WriteLine(" A soma de {0} + {1} = {2}", num1, num2, num1 + num2);
+
+            num1 = 45;
+            num2 = 35;
+            num3 = num1 - num2;
+
+            Console.WriteLine("A  substração de {0} - {1} = {2}", num1, num2, num3);
+
+            num1 = 45;
+            num2 = 35;
+            num3 = num1 * num2;
+
+            Console.WriteLine("A  multiplicação de {0} * {1} = {2}", num1, num2, num3);
+
+            num1 = 45;
+            num2 = 35;
+            resposta = num1 / num2;
+
+            Console.WriteLine("A divisão de {0} / {1} = {2}", num1, num2, resposta);
+
+            resposta = num1 % num2;
+
+            Console.WriteLine("O resto da divisão de {0} % {1} = {2}", num1, num2, resposta);
+
+            num4 = 35f;
+            resposta = num1 / num4;
+
+            Console.WriteLine("A divisão de {0} / {1} = {2}", num1, num4, resposta);
+        }
+    }
+}
+
+
---
+++ Ex005/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int num;
+
+            Console.WriteLine("Digite um número;");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} x 1 = {1}", num, num * 1);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 2);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 3);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 4);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 5);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 6);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 7);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 8);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 9);
+            Console.WriteLine("{0} x 1 = {1}", num, num * 10);
+
+
+         }
+    }
+}
+

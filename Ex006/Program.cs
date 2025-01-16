c7e6caad2b493afeb6ea2e9929ee3f67b16f1412
WELLINGTON AMARANTE ALVES <wellington.aalves4@senacsp.edu.br>
2025-01-15 21:28:16 -0300

    Atualização do execício

---
+++ Ex006/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int resposta, num3;
+
+            Console.WriteLine("Digite uma nota");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite outra nota");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3= num1 + num2;
+            resposta = num3 / 2;
+            Console.WriteLine("a média de {0}", resposta);
+
+
+        }   
+    }
+}

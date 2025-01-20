49d7ec664ffa2b3384c5b56b5360bf784e09afc1
WELLINGTON AMARANTE ALVES <wellington.aalves4@senacsp.edu.br>
2025-01-20 19:41:25 -0300

    Exercicio do teste dp triangulo

---
+++ Ex009/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args);
+
+        double número;
+        string resultado;
+        
+
+      Console.WriteLine("Digite um número");
+            valor = Convert.ToInt32
+
+            if (número % 2==0)
+            {
+            
+
+
+
+        }
+    }
+}

40402c2e5d2b674aa7391e8f5cbece9db024f70f
WELLINGTON AMARANTE ALVES <wellington.aalves4@senacsp.edu.br>
2025-01-20 19:41:06 -0300

    Exercício do teste do triângulo

---
+++ Ex008/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidade;
+            string resultado;
+            Console.WriteLine("Qual a velocidade do carro");
+            velocidade = Convert.ToDouble(Console.ReadLine());
+
+            if (velocidade > 80)
+            {
+                velocidade = (velocidade - 80) * 7;
+                Console.WriteLine("Você foi multado + Você foi multado em R$ {0}", velocidade);
+            }
+
+            else
+            {
+                Console.WriteLine ("Velocidade normal");
+            }
+          
+        }
+
+    }
+}
+

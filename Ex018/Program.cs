6926c27207663056f2c5d6ceed6279b84b5f1971
WELLINGTON AMARANTE ALVES <wellington.aalves4@senacsp.edu.br>
2025-01-29 22:20:22 -0300

    Exercicios de aula

---
+++ Ex018/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Calcular a Àrea do Triângulo");
+            Console.WriteLine("==========================");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a base do triângulo(m): ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a altura do triângulo(m): ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+    
+            area(a, b);
+        }
+
+        static void area(double bas, double altura)
+        {
+            double a = (bas * altura) / 2;
+            Console.WriteLine($"A área do triângulo de {bas} * {altura}/ 2 é de {a:F2}m²");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.Design;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o operador (1=+, 2=-, 3=*, 4=/ ");
+            string conta = Console.ReadLine();
+
+            if (conta == "+")
+            {
+                soma(num1, num2);
+            }
+            else if (conta == "-")
+            {
+                sub(num1, num2);
+            }
+            else if (conta == "*")
+            {
+                multiplicacao(num1, num2);
+            }
+            else if (conta == "/")
+            {
+                divisao(num1, num2);
+            }
+
+
+            static void soma(int a, int b)
+            {
+                int resultado = a + b;
+                Console.WriteLine("A soma de " + a + " + " + b + " = " + resultado);
+            }
+
+
+            static void sub(int a, int b)
+            {
+                int resultado = a - b;
+                Console.WriteLine("A subtração de " + a + " - " + b + " = " + resultado);
+            }
+
+            static void multiplicacao(int a, int b)
+            {
+                int resultado = a + b;
+                Console.WriteLine("A multiplicação de " + a + " * " + b + " = " + resultado);
+            }
+
+            static void divisao(int a, int b)
+            {
+                int resultado = a + b;
+                Console.WriteLine("A divisão de " + a + " / " + b + " = " + resultado);
+            }
+        }
+    }
+}

8ec442a95edc24da80a11b7fc968b1cbdda5f01f
WELLINGTON AMARANTE ALVES <wellington.aalves4@senacsp.edu.br>
2025-01-29 21:06:54 -0300

    Aula 29/01/2025

---
+++ Formatacao/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Formatacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 10.54821;
+            double num2 = 54781.65874;
+
+            Console.WriteLine($"{num1:F2}"); // 4 casas decimais
+            Console.WriteLine($"{num2:N2}"); // separador de milhar
+            Console.WriteLine($"{num1:C2}"); // moeda
+            Console.WriteLine($"{num2:E2}"); // notação cientifica
+            Console.WriteLine($"{num1:P2}"); // porcentagem
+        }
+    }
+}
---
+++ Impressao/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Impressao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            escreva("Wellington Amarante Alves");
+            Console.WriteLine();
+            escreva("Curso C#");
+            Console.WriteLine();
+            escreva("Olá");
+            Console.WriteLine();
+        }
+
+        static void escreva(string texto)
+        {
+            /* Escreva o texto centralizado em uma linha com o caracter informado*/
+
+            char caracter = '='; // Caracter que será impresso
+            int tamanho = texto.Length + 4; // Tamanho da linha
+            string linha = new string(caracter, tamanho); // Cria uma linha com o caracter informado
+            string textoCentralizado = texto.PadLeft(2 + texto.Length).PadRight(tamanho); // Centraliza o texto
+            Console.WriteLine(linha);
+            Console.WriteLine(textoCentralizado);
+            Console.WriteLine(linha);
+        }
+    }
+}
